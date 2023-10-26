5b83ff780d986fbb31ffc8e97883f66bcecbf9ec
Inigo de Robina <iderobina@MacBook-Air-2.local>
2023-10-25 18:01:17 -0600

    not tested yet, finished project.

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
-const {connect, connection} = require('mongoose');
+const mongoose = require('mongoose');
 
-connect('mongodb://localhost:27017')
 
-module.exports = connection;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialmedia", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+module.exports = mongoose.connection;
